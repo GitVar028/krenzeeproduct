cebb6bf907714353748902d8d26ec07e2c23294d
vavillarino <vavillarino@yondu.com>
2026-05-05 12:23:09 +0800

    - fix API Gateway proxy handler

--- src/main/java/com/krayzee/krenzeeproduct/adapter/configuration/StreamLambdaHandler.java
+++ src/main/java/com/krayzee/krenzeeproduct/adapter/configuration/StreamLambdaHandler.java
@@ -1,8 +1,8 @@
 package com.krayzee.krenzeeproduct.adapter.configuration;
 
 import com.amazonaws.serverless.exceptions.ContainerInitializationException;
-import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
 import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
+import com.amazonaws.serverless.proxy.model.HttpApiV2ProxyRequest;
 import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
@@ -13,21 +13,18 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 public class StreamLambdaHandler implements RequestStreamHandler {
-
-    private static final SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
-
+    
+    private static SpringBootLambdaContainerHandler<HttpApiV2ProxyRequest, AwsProxyResponse> handler;
+    
     static {
         try {
-            // Manual initialization
-            handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(KrenzeeproductApplication.class);
-            
-            // This is the key: it tells the handler to accept multiple types of events
-            // including the HTTP API v2 that was failing for you earlier.
+            // Use getHttpApiV2ProxyHandler instead of getAwsProxyHandler
+            handler = SpringBootLambdaContainerHandler.getHttpApiV2ProxyHandler(KrenzeeproductApplication.class);
         } catch (ContainerInitializationException e) {
             throw new RuntimeException("Could not initialize Spring Boot application", e);
         }
     }
-
+    
     @Override
     public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context)
             throws IOException {
